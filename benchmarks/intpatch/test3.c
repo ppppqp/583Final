cdc5bfca9996e08d9588f20b85714ff9e128941a
Qiping Pan <panqp@eecs583a.eecs.umich.edu>
2023-03-29 09:40:56 -0400

    test3

---
+++ benchmarks/intpatch/test3.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h> 
+void fn(int param1){
+    return;
+}
+int main(){
+    char charArray[100];
+    int input;
+    FILE* pFile;
+    pFile = fopen("input1", "r");
+    fscanf(pFile, "%d", &input);
+    printf("%d", input);
+    for(int i = 0; i < 100; i++){
+        input++;
+    }
+    printf("%d", input);
+    fclose(pFile);
+    return 0;
+}
